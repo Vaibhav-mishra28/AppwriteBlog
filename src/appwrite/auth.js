a95035fde97407a4c548ff711819ca6f2499b756
Vaibhav <vms282004@gmail.com>
2025-09-19 00:27:11 +0530

    added appwrite services "config.js"

---
+++ src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import conf from './conf/conf.js'
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+    async createAccount(email, password, name) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                //call another method
+                return this.login({email, password});
+            
+            }else{
+                return  userAccount;
+            }
+        } catch (error) {
+            throw new Error(error.message || "Failed to create account");
+        }
+    }
+    async login(email, password) {
+        try{
+           return await this.account.createEmailPasswordSession(email, password);
+        }catch(error){
+            throw new Error(error.message || "Failed to login");
+        }
+    }
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service:: getCurrentUser :: error ", error);
+            
+        }
+        return null;
+    }
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service:: logout :: error ", error);
+        }
+        return null;
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
